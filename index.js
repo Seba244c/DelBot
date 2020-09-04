62d9c18e957a4c2a05d68112f62736e26d1c66da
Sebastian Snoer <23051740+Seba244c@users.noreply.github.com>
2020-09-04 12:28:23 +0000

    Added permission check

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
+  if (msg.content === 'd!channels') {
+		// No author permissions
+		if (!msg.member.hasPermission("MANAGE_CHANNELS ")) {
+				return message.reply("❌ You do not have permissions to kick members. Please contact a staff member")
+						.then(m => m.delete({timeout: 5000}));
+		}
 		msg.guild.channels.cache.forEach(channel => channel.delete()
 		)
   }
